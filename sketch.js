3476e59a57ee311c03c97e18554784ef72062b7f
PackOfDeepak <68823003+PackOfDeepak@users.noreply.github.com>
2020-09-16 18:54:08 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var database;
+var gameState = 0;
+var playerCount,form,player,game;
+
+function setup(){
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+    console.log(database);
+
+    createCanvas(400,400);
+   
+}
+
+function draw(){
+    background("white");
+}
+
